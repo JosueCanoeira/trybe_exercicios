e9fa3f8ed584feef84435890b04449b8a04ab81a
Josue Canoeira <titocanoeira@gmail.com>
2021-11-18 10:34:59 +0100

    adiciona os arquivos js

---
+++ fundamentos/bloco-04-introducao-a-JavaScript-e-Lógica-de-Programação/aula-04-javascript-objetos-e-Funcoes/exercise2.js
@@ -0,0 +1,12 @@
+let diasDaSemana = {
+1: "Domingo",
+2: "Segunda",
+3: "Terça",
+4: "Quarta",
+5: "Quinta",
+6: "Sexta",
+7: "Sábado",
+};
+
+console.log(diasDaSemana["1"] + " " + diasDaSemana["2"] + " " + diasDaSemana["3"] + " " + diasDaSemana["4"] + " " + diasDaSemana["5"] + " " + diasDaSemana["6"] + " " + diasDaSemana["7"]);
+console.table(diasDaSemana); 
