d50ba068d357d09ee89d596d9205d9c5fc857a76
Josue Canoeira <you@example.com>
2022-03-15 21:05:06 +0100

    adiciona as alteraçoes do exe1

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/exer1.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/exer1.js
@@ -24,20 +24,3 @@ const newEmployees = (callback) => {
   return employees;
 };
 console.log(newEmployees(createEmail));
-
-/////////////////////////////////////////////////////////////////////////////////////////////
-/*2 - Desenvolva uma HOF que retorna o resultado de um sorteio. Esta HOF irá gerar um número 
-aleatório entre 1 e 5 recebendo como parâmetros o número apostado e uma função que checa se
- o número apostado é igual ao número sorteado. O retorno da sua HOF deve ser uma 
- string (Ex: "Tente novamente" ou "Parabéns você ganhou").*/
-
-/*3 - Crie uma HOF que receberá três parâmetros. O primeiro será um array de respostas
-corretas (Gabarito), o segundo será um array de respostas a serem verificadas 
-(respostas da pessoa estudante) e o terceiro é uma função que checa se as respostas estão
-corretas e faz a contagem da pontuação final recebida pela pessoa estudante. Ao final a 
- HOF deve retornar o total da contagem de respostas certas.
-Quando a resposta for correta a contagem sobe 1 ponto, quando for incorreta desce 0.5 pontos,
-e quando não houver resposta ("N.A") não altera-se a contagem.
-Copiar
-const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
-const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];*/
