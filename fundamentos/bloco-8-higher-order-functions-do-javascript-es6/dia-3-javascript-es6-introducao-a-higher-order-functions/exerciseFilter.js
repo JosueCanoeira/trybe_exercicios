5ddec8b6c5a92d790cbc01a2c1755894b55f8a8e
Josue Canoeira <you@example.com>
2022-03-22 06:51:38 +0100

    adiciona novos execicios

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-introducao-a-higher-order-functions/exerciseFilter.js
@@ -0,0 +1,23 @@
+const listaFrutas = [
+  "Laranja",
+  "Limão",
+  "Abacate",
+  "Laranja",
+  "Pera",
+  "Laranja",
+  "Maçã",
+  "Abacate",
+];
+
+const filtrarMaçã = (array) => array.filter((element) => element === "Laranja");
+console.log(filtrarMaçã(listaFrutas));
+
+
+setTimeout(() => {
+  console.log('Comprar parafusos') // Comprar parafusos
+  console.log('Adicionar ao estoque') // Adicionar ao estoque
+}, 2000);
+
+console.log('1 - Receber roda'); // 1 - Receber roda
+console.log('2 - Encaixar parafusos'); // 2 - Encaixar parafusos
+console.log('3 - Fixar roda no carro'); // 3 - Fixar roda no carro
